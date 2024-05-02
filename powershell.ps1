13f0d73982ee35a05e9e3b8ce8cf2f31b28af888
M4rshe1 <colin.heggli@gmail.com>
2024-05-02 12:33:35 +0200

    powershell command fixes

--- powershell.ps1
+++ powershell.ps1
@@ -1,15 +1,15 @@
-function bin{
+function bin
+{
     param(
         [string]$path
     )
     $ServerURI = "https://bin.heggli.dev"
     $token = "laskjdflaskjdfhalskdjfhlaksdjfhlkasjsdliksdehbuioerr"
     $content = Get-Content $path -Raw
-    # send a request to 127.0.0.1:8080 with the content as parameter file
     $body = @{
         file = $content
-        filename = $path.Split("\")[-1].Split("/")[-1],
+        filename = $path.Split("\")[-1].Split("/")[-1]
         token = $token
     }
-    Invoke-RestMethod -Uri "http://127.0.0.1:8000" -Method Get -Body $body
+    Invoke-RestMethod -Uri $ServerURI -Method Get -Body $body
 }
